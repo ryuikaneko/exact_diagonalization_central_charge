338ce1357eb4cef23ff3e6d47ab220a30cc83a79
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2019-12-06 10:46:51 +0900

    add data

---
+++ 1D_Heisenberg/dat/AAA_run_L8_to_L32.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+prog=../src/no_eigvec.py
+#prog=../src/no_eigvec_no_matrix.py
+
+for L in \
+8
+#8 10 12 14 16 18 20 22 24 26 28 30 32
+#6 ## too small for scipy.sparse.linalg.eigsh
+do
+
+for momk in \
+`seq 0 $((L/2))`
+do
+
+date
+
+echo ${L} ${momk}
+python2.7 ${prog} -L ${L} -momk ${momk} > dat_L${L}_momk${momk}
+
+date
+
+done
+
+done
