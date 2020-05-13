cb1c49511d2cddba1881af9aec81f52117631aba
Ryui Kaneko <27846552+ryuikaneko@users.noreply.github.com>
2020-05-13 12:55:27 +0900

    add 1d TFI data

---
+++ 1d_TransverseFieldIsing/dat/AAA_run_L3_to_L24.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+prog=../src/no_eigvec.py
+#prog=../src/no_eigvec_no_matrix.py
+
+for L in \
+`seq 3 25`
+do
+
+for momk in \
+`seq 0 $((L/2))`
+do
+
+date
+
+echo ${L} ${momk}
+python ${prog} -L ${L} -momk ${momk} > dat_L${L}_momk${momk}
+
+date
+
+done
+
+done
